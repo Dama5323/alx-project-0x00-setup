c9dc4e6be8923b62cd2e737df0e32e22311cdb5b
Dama5323 <your-deenyashke@gmail.com>
2025-11-16 03:11:30 +0300

    feat: integrate Card component into landing page

--- alx-project-0x00/components/Card.tsx
+++ alx-project-0x00/components/Card.tsx
@@ -6,8 +6,8 @@ import Pill from "./Pill"
 
 const Card: React.FC = () => {
   return (
-    <div className="h-[422px] w-[378.56px] cursor-pointer hover:shadow-md hover:rounded-lg ">
-      <Image className="rounded-lg" src={HOUSE_IMAGE} width={378.56} height={299.37} alt="house image" />
+    <div className="h-[422px] w-[378.56px] cursor-pointer hover:shadow-md hover:rounded-md "> {/* Changed from rounded-lg */}
+      <Image className="rounded-md" src={HOUSE_IMAGE} width={378.56} height={299.37} alt="house image" /> {/* Changed from rounded-lg */}
       <div className="p-2 flex gap-2 mt-2">
         <Pill title="Top Villa" />
         <Pill title="Self CheckIn" />
